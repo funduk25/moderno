edb006124d2eaf59807e0b115352159bedd9a6ef
funduk25 <slender330@mail.ru>
2020-03-08 18:05:33 +0700

    added product  slider

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,18 @@ $(function() {
 
     var mixer = mixitup('.product__inner-box');
 
+    $(".rate-star").rateYo({
+        rating: 5,
+        starWidth: "12px",
+        readOnly: 5
+    });
 
+    $('.product__slider-inner').slick({
+        dots: true,
+        infinite: false,
+        arrows: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
 });
